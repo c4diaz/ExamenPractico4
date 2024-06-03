7b78a8ae80b549b51dac3235c5eb30ca84a86323
c4diaz <CARLOS DIAZ@DESKTOP-91AU5N9>
2024-06-03 12:28:04 +0200

    Clases Persona y Empleado creadas

---
+++ src/main/java/com/example/Empleado.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@ToString (callSuper = true)
+
+
+
+public class Empleado extends Persona {
+
+	private Departamento departamento;
+	private double salario;
+	
+}
